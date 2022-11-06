bb0568a1eb8f11c84bc2c70747789350cc49f8ce
Johannes Bauer <joe@johannes-bauer.com>
2022-11-06 10:43:04 +0100

    Close requests.Session() in the example
    When we run *lots* of queries, we might run out of sockets to use
    because they're all lingering in TIME_WAIT because sessions are not
    properly closed. Do it properly in the example.

--- labwork/my_solution.py
+++ labwork/my_solution.py
@@ -22,37 +22,37 @@ def handle_strcat(assignment):
 def handle_foobar(assignment):
 	return { "foo": "bar" }
 
-session = requests.Session()
-# Get the assignment
-result = session.get(api_endpoint + "/assignment/" + client_id + "/" + assignment_name)
-assert(result.status_code == 200)
-
-# See if we can compute the answer
-assignment = result.json()
-known_assignment_count = 0
-unknown_assignment_count = 0
-pass_count = 0
-for testcase in assignment["testcases"]:
-	if testcase["type"] == "strcat":
-		known_assignment_count += 1
-		response = handle_strcat(testcase["assignment"])
-	elif testcase["type"] == "foobar":
-		known_assignment_count += 1
-		response = handle_foobar(testcase["assignment"])
-	else:
-		unknown_assignment_count += 1
-		print("Do not know how to handle type: %s" % (testcase["type"]))
-		continue
-
-	# We think we have an answer for this one, try to submit it
-	result = session.post(api_endpoint + "/submission/" + testcase["tcid"], headers = {
-		"Content-Type": "application/json",
-	}, data = json.dumps(response))
+with requests.Session() as session:
+	# Get the assignment
+	result = session.get(api_endpoint + "/assignment/" + client_id + "/" + assignment_name)
 	assert(result.status_code == 200)
-	submission_result = result.json()
-	if submission_result["status"] == "pass":
-		pass_count += 1
-	else:
-		print(submission_result)
-print("%d known assignments, %d unknown." % (known_assignment_count, unknown_assignment_count))
-print("Passed: %d. Failed: %d" % (pass_count, known_assignment_count - pass_count))
+
+	# See if we can compute the answer
+	assignment = result.json()
+	known_assignment_count = 0
+	unknown_assignment_count = 0
+	pass_count = 0
+	for testcase in assignment["testcases"]:
+		if testcase["type"] == "strcat":
+			known_assignment_count += 1
+			response = handle_strcat(testcase["assignment"])
+		elif testcase["type"] == "foobar":
+			known_assignment_count += 1
+			response = handle_foobar(testcase["assignment"])
+		else:
+			unknown_assignment_count += 1
+			print("Do not know how to handle type: %s" % (testcase["type"]))
+			continue
+
+		# We think we have an answer for this one, try to submit it
+		result = session.post(api_endpoint + "/submission/" + testcase["tcid"], headers = {
+			"Content-Type": "application/json",
+		}, data = json.dumps(response))
+		assert(result.status_code == 200)
+		submission_result = result.json()
+		if submission_result["status"] == "pass":
+			pass_count += 1
+		else:
+			print(submission_result)
+	print("%d known assignments, %d unknown." % (known_assignment_count, unknown_assignment_count))
+	print("Passed: %d. Failed: %d" % (pass_count, known_assignment_count - pass_count))
